3b9efee3486fa346653017fdc5270cf34bbc1192
Deviganesan <51699366+Deviganesan@users.noreply.github.com>
2019-07-13 19:35:59 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    char str;
+    scanf("%c",&str);
+    if(str=='1'||str=='2'||str=='3'||str=='4'||str=='5'||str=='6'||str=='7'||str=='8'||str=='9'||str=='0')
+        printf("yes");
+    else
+        printf("No");
+}
