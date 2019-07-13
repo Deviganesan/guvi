73df01967591e74937d4d381dd01a0e920c49f33
Deviganesan <51699366+Deviganesan@users.noreply.github.com>
2019-07-13 19:25:04 +0530

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int temp;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        printf("%d ",arr[i]);
+    }
+}
