6b8f622198c0954f46becef2e051ef4d6fc0072a
Deviganesan <51699366+Deviganesan@users.noreply.github.com>
2019-07-16 14:47:40 +0530

    Create character.py

---
+++ character.py
@@ -0,0 +1,8 @@
+test_string=input(" ")
+c=0
+for word in test_string:
+  
+  if word=='1'or word=='2'or word=='3'or word=='4'or word=='5'or word=='6'or word=='7'or word=='8'or word=='9':
+    c+=1
+  
+print(c) 
